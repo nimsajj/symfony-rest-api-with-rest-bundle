ca8e99e5e20cbfa7c76a0fdeb6a0bd7f3407185a
nimsajj <nimsaj.dev@gmail.com>
2020-11-13 15:19:25 +0100

    install fosAuthServer dependencie

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
 ];
